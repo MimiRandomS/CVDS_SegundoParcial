43d5de40cfc512426b186669cf50a6508bbe00de
unknown <geronimo.martinez-n@labinfo.is.escuelaing.edu.co>
2025-03-27 15:56:00 -0500

    -

--- src/main/java/edu/eci/cvds/ECICredit/config/CorsConfig.java
+++ src/main/java/edu/eci/cvds/ECICredit/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://parcialecicredit-cnfxd0b0f3c4aabz.brazilsouth-01.azurewebsites.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
